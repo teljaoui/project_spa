ed40188f303ea97ed1b41941856066befae62b04
teljaoui <teljaoui2004@gmail.com>
2024-12-26 14:33:38 +0100

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,69 +13,13 @@
 </head>
 
 <body>
-    <header>
-        <nav class="header-top">
-            <div>
-                <ul>
-                    <li><a href=""><i class="fa-solid fa-location-dot"></i> Test , Test , N00</a></li>
-                    <li><a href=""><i class="fa-solid fa-envelope"></i> info@gmail.com</a></li>
-                </ul>
-                <ul>
-                    <li><a href=""><i class="fa-solid fa-phone"></i> +212 652583234</a></li>
-                    <li><a href=""><i class="fa-brands fa-whatsapp"></i> +212 652583234</a></li>
-                </ul>
-            </div>
-        </nav>
-        <nav class="header-bottom">
-            <div>
-                <ul>
-                    <li class="logo">
-                        <a href="/" onclick="shownavhome()"><img src="img/logotop.png"
-                                alt=""srcset=""><span>Spa</span></a>
-                        <button class="xmark" onclick="closenavhome()"><i class="fa-solid fa-xmark"></i></button>
-                    </li>
-                </ul>
-                <ul>
-                    <li><a href="/" class="navigation" onclick="closenavhome()">Home <i
-                                class="fa-solid fa-arrow-right"></i></a></li>
-                    <li><a href="#services" class="navigation" onclick="closenavhome()">Service <i
-                                class="fa-solid fa-arrow-right"></i></a></li>
-                    <li><a href="#home-contact" class="navigation" onclick="closenavhome()">Contact <i
-                                class="fa-solid fa-arrow-right"></i></a></li>
-                </ul>
-                <ul>
-                    <li class="reserve"><a href="" onclick="closenavhome()">Réserver</a></li>
-                </ul>
-                <ul class="contact">
-                    <hr class="mx-4 bg-light my-3">
-                    <div>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-solid fa-envelope"></i></a></a></li>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-solid fa-phone"></i></a></li>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-brands fa-whatsapp"></i></a></li>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-brands fa-instagram"></i></a></li>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-brands fa-facebook"></i></a></li>
-                        <li><a href="" onclick="closenavhome()"><i class="fa-brands fa-telegram"></i></a></li>
-                    </div>
-                </ul>
-            </div>
-        </nav>
-        <nav class="phone">
-            <ul class="px-3 pt-2">
-                <li class="logo"><a href=""><img src="img/logotop.png" alt=""
-                            srcset=""><span>Spa</span></a>
-                </li>
-                <li>
-                    <button onclick="shownavhome()"><i class="fa-solid fa-bars"></i></button>
-                </li>
-            </ul>
-        </nav>
-    </header>
+   @include('layout/header')
     <section class="home-top" id="home-top">
         <div class="content">
             <h5>Beauty Services</h5>
             <p>Give yourself this moment . <br>
                 make an appointment in just a click</p><br>
-            <a href="">make an appointment</a>
+            <a href="/appointment">make an appointment</a>
         </div>
     </section>
     <section class="home-service" id="services">
@@ -152,48 +96,8 @@
             </div>
         </div>
     </section>
+    @include('layout/footer')
 
-    <footer>
-        <div class="container py-5">
-            <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-12">
-                    <div class="logo pb-3">
-                        <a href="/"><img src="img/logo.jpg" alt=""srcset=""></a>
-                    </div>
-                    <div>
-                        <p>Welcome to one of the finest salon and spa experiences in your area, where relaxation and
-                            rejuvenation await!</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-12">
-                    <h5>Office</h5>
-                    <li><a href=""><i class="fa-solid fa-location-dot"></i>Test , Test , N00</a></li>
-                    <li><a href=""><i class="fa-solid fa-envelope"></i>info@gmail.com</a></li>
-                    <li><a href=""><i class="fa-solid fa-phone"></i>+212 652583234</a></li>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-12">
-                    <h5>Links</h5>
-                    <li><a href="/" class="navigation">Home</a></li>
-                    <li><a href="#services" class="navigation">Service</a>
-                    </li>
-                    <li><a href="#home-contact" class="navigation">Contact Us</a></li>
-                    <li><a href="">Make An Appointment</a></li>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-12 media">
-                    <h5>Get In Touch</h5>
-                    <li><a href=""><i class="fa-brands fa-whatsapp"></i>+212 652583234</a></li>
-                    <li><a href=""><i class="fa-brands fa-instagram"></i>test_test</a></li>
-                    <li><a href=""><i class="fa-brands fa-facebook"></i>Test Test</a></li>
-                    <li><a href=""><i class="fa-brands fa-telegram"></i>+212 652583234</a></li>
-                </div>
-
-            </div>
-        </div>
-        <div class="py-4 w-100">
-            <p class="mx-auto fs-5">Teljaoui Mohamed © 2024. All Rights Reserved.</p>
-        </div>
-    </footer>
-   
 
     <script src="main.js"></script>
 
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,40 @@
+<footer>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="logo pb-3">
+                    <a href="/"><img src="img/logo.jpg" alt=""srcset=""></a>
+                </div>
+                <div>
+                    <p>Welcome to one of the finest salon and spa experiences in your area, where relaxation and
+                        rejuvenation await!</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <h5>Office</h5>
+                <li><a href=""><i class="fa-solid fa-location-dot"></i>Test , Test , N00</a></li>
+                <li><a href=""><i class="fa-solid fa-envelope"></i>info@gmail.com</a></li>
+                <li><a href=""><i class="fa-solid fa-phone"></i>+212 652583234</a></li>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <h5>Links</h5>
+                <li><a href="/" class="navigation">Home</a></li>
+                <li><a href="#services" class="navigation">Service</a>
+                </li>
+                <li><a href="#home-contact" class="navigation">Contact Us</a></li>
+                <li><a href="">Make An Appointment</a></li>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 media">
+                <h5>Get In Touch</h5>
+                <li><a href=""><i class="fa-brands fa-whatsapp"></i>+212 652583234</a></li>
+                <li><a href=""><i class="fa-brands fa-instagram"></i>test_test</a></li>
+                <li><a href=""><i class="fa-brands fa-facebook"></i>Test Test</a></li>
+                <li><a href=""><i class="fa-brands fa-telegram"></i>+212 652583234</a></li>
+            </div>
+        </div>
+        <div class="pt-4 w-100">
+            <p class="fs-6">Teljaoui Mohamed © 2024. All Rights Reserved.</p>
+        </div>
+    </div>
+
+</footer>
