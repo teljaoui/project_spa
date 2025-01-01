83f4dcdfb7ffbed595b03c2935620b4eb43c13bb
teljaoui <teljaoui2004@gmail.com>
2025-01-01 13:49:04 +0100

    up

--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -33,101 +33,108 @@
                     </form>
                 </div>
             </div>
-            <div class="today admin-time my-1">
-                <h6 class="title">Appointment Booking</h6>
-                <div>
-                    <div class="form-group">
-                        <label for="">Here are the available time slots. Click on a slot to proceed with
-                            booking.</label>
-                        <div class="time my-4 text-center">
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
-                            <form action="" method="post">
-                                <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
-                            </form>
+            @if (session('date'))
+                <div class="today admin-time my-1">
+                    <h6 class="title">Appointment Booking</h6>
+                    <div>
+                        <div class="form-group">
+                            <label for="">Here are the available time slots. Click on a slot to proceed with
+                                booking.</label>
+                            <div class="time my-4 text-center">
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
+                                <form action="" method="post">
+                                    <button><i class="fa-regular fa-circle"></i><span>10:00 am</span></button>
+                                </form>
 
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="today">
-                <h6 class="title">Appointment Booking</h6>
-                <div>
-                    <form action="">
-                        <div class="">
-                            <div class="form-group">
-                                <label for="">Booking confirmed for 4:00 PM, Dec 27, 2024.
-                                    Kindly fill in your details below to proceed.</label>
-                                <div class="row my-3">
-                                    <div class="col-lg-6 col-md-12 col-sm-12 my-2">
-                                        <label for="" class="form-label">First Name</label>
-                                        <input type="text" class="form-control" name="firstname" required>
-                                    </div>
-                                    <div class="col-lg-6 col-md-12 col-sm-12 my-2">
-                                        <label for="" class="form-label">Last Name</label>
-                                        <input type="text" class="form-control" name="lastname" required>
-                                    </div>
-                                    <div class="col-lg-12 col-md-12 col-sm-12 my-2">
-                                        <label for="" class="form-label">Phone Number</label>
-                                        <input type="text" class="form-control" name="number" required>
+            @endif
+            @if (session('time'))
+                <div class="today">
+                    <h6 class="title">Appointment Booking</h6>
+                    <div>
+                        <form action="">
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="">Booking confirmed for 4:00 PM, Dec 27, 2024.
+                                        Kindly fill in your details below to proceed.</label>
+                                    <div class="row my-3">
+                                        <div class="col-lg-6 col-md-12 col-sm-12 my-2">
+                                            <label for="" class="form-label">First Name</label>
+                                            <input type="text" class="form-control" name="firstname" required>
+                                        </div>
+                                        <div class="col-lg-6 col-md-12 col-sm-12 my-2">
+                                            <label for="" class="form-label">Last Name</label>
+                                            <input type="text" class="form-control" name="lastname" required>
+                                        </div>
+                                        <div class="col-lg-12 col-md-12 col-sm-12 my-2">
+                                            <label for="" class="form-label">Phone Number</label>
+                                            <input type="text" class="form-control" name="number" required>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="d-flex justify-content-center my-3">
-                            <input type="submit" value="Next" class="w-50 submit">
-                        </div>
-                    </form>
+                            <div class="d-flex justify-content-center my-3">
+                                <input type="submit" value="Next" class="w-50 submit">
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <div class="today my-1">
-                <h6 class="title">Appointment Booking</h6>
-                <div>
-                    <form action="">
-                        <p>Check the information entered.</p>
-                        <div class="mb-4">
-                            <a href="" class="back"><i class="fa-solid fa-arrow-left"></i> Back</a>
-                        </div>
-                        <ul>
-                            <li>
-                                <span>Date Of Visit:</span>
-                                <p>12/12/2025</p>
-                            </li>
-                            <li>
-                                <span>Visiting Time:</span>
-                                <p>12:00 am</p>
-                            </li>
-                            <li>
-                                <span>First Name:</span>
-                                <p>Mohamed</p>
-                            </li>
-                            <li>
-                                <span>Last Name:</span>
-                                <p>Teljaoui</p>
-                            </li>
-                            <li>
-                                <span>Phone Number:</span>
-                                <p>0652583234</p>
-                            </li>
-                        </ul>
-                        <span class="admin-span">If the information is correct click on the button below</span><br><br>
-                        <button type="submit" class="next">Confirmed</button>
-                    </form>
+            @endif
+            @if (session('date') && session('time'))
+                <div class="today my-1">
+                    <h6 class="title">Appointment Booking</h6>
+                    <div>
+                        <form action="">
+                            <p>Check the information entered.</p>
+                            <div class="mb-4">
+                                <a href="" class="back"><i class="fa-solid fa-arrow-left"></i> Back</a>
+                            </div>
+                            <ul>
+                                <li>
+                                    <span>Date Of Visit:</span>
+                                    <p>12/12/2025</p>
+                                </li>
+                                <li>
+                                    <span>Visiting Time:</span>
+                                    <p>12:00 am</p>
+                                </li>
+                                <li>
+                                    <span>First Name:</span>
+                                    <p>Mohamed</p>
+                                </li>
+                                <li>
+                                    <span>Last Name:</span>
+                                    <p>Teljaoui</p>
+                                </li>
+                                <li>
+                                    <span>Phone Number:</span>
+                                    <p>0652583234</p>
+                                </li>
+                            </ul>
+                            <span class="admin-span">If the information is correct click on the button
+                                below</span><br><br>
+                            <button type="submit" class="next">Confirmed</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </section>
 
